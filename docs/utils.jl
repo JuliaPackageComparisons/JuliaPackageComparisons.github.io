5d058378121b767c1c8d4eee109ba7850bf3efff
Thomas Poulsen <ta.poulsen@gmail.com>
2025-08-10 11:48:02 +0200

    fix no docs of CBinding

--- docs/utils.jl
+++ docs/utils.jl
@@ -116,7 +116,7 @@ const PKGINFOS = [
     PkgInfo(pkgname="WaterLily", username="weymouth", branch="master", docslink="https://waterlily-jl.github.io/WaterLily.jl/dev/"),
     PkgInfo(pkgname="Oceananigans", username="CliMA", branch="main", docslink="https://clima.github.io/OceananigansDocumentation/stable/"),
     PkgInfo(pkgname="Clang", username="JuliaInterop", branch="master"),
-    PkgInfo(pkgname="CBinding", username="analytech-solutions", branch="master"),
+    PkgInfo(pkgname="CBinding", username="analytech-solutions", branch="master", docslink=nothing),
     PkgInfo(pkgname="Cxx", username="JuliaInterop", branch="master"),
     PkgInfo(pkgname="CxxWrap", username="JuliaInterop", docslink=nothing),
     PkgInfo(pkgname="PerfChecker", username="JuliaConstraints", docslink=nothing),
